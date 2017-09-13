c590e7e9cb77ab7ced053643b3f134958b886470
Atul Kumar <atulkumarpccs@users.noreply.github.com>
2017-09-13 11:42:54 +1000

    Create RecordReceipt.h

---
+++ DomainClass/RecordReceipt.h
@@ -0,0 +1,23 @@
+#ifndef RECORDRECEPIT_H
+#define RECORDRECEPIT_H
+
+#include <Vehicle.h>
+#include <Security.h>
+#include <ParkPrice.h>
+
+class RecordReceipt :public Vehicle, public Security,public ParkPrice
+{
+
+private:
+
+	Entrytime();
+	ExitTime();
+	void TotalTime();
+	int TotalChages();
+	int Parking Location();
+
+
+};
+
+#endif // !RECORDRECEPIT_H
+
