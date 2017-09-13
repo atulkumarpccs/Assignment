07aede7a46e413996c7856faf838020d46cfcfaa
Atul Kumar <atulkumarpccs@users.noreply.github.com>
2017-09-13 11:45:22 +1000

    Create Kisok.h

---
+++ DomainClass/Kisok.h
@@ -0,0 +1,23 @@
+/*
+*This file is introduced for the kisok use in parking system which would be 
+*/
+#ifndef KISOK_H
+#define KISOK_H
+
+#include<CarPark>
+class Kisok :public CarPark
+{
+
+public:
+	/*
+	*TODO: Overdide  some of the functionality here of business class here specific to kisok support or limitation
+	*/
+
+protected:
+
+private:
+
+
+
+};
+#endif /*KISOK */
